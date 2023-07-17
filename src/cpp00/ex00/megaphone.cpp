c68f8ee9485a4455fa1c40d6c1ac1bf6816a1d85
Anthony Tsang <htsang@student.42heilbronn.de>
2023-07-17 23:37:23 +0200

    chore(cpp00 ex00): Add namespace to avoid name clashing

--- src/cpp00/ex00/megaphone.cpp
+++ src/cpp00/ex00/megaphone.cpp
@@ -6,16 +6,21 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:07:56 by htsang            #+#    #+#             */
-/*   Updated: 2023/07/13 16:47:24 by htsang           ###   ########.fr       */
+/*   Updated: 2023/07/17 16:09:40 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char	toUpper(unsigned char c)
+namespace megaphone
+{
+
+char	toupper(unsigned char c)
 {
 	return (static_cast<char>(std::toupper(c)));
 }
+	
+} // namespace megaphone
 
 int main(int argc, char const *argv[])
 {
@@ -28,7 +33,7 @@ int main(int argc, char const *argv[])
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
-				std::cout << toUpper(static_cast<unsigned char>(argv[i][j]));
+				std::cout << megaphone::toupper(static_cast<unsigned char>(argv[i][j]));
 		}
 	}
 	std::cout << std::endl;
