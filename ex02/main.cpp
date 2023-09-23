5163d1b869e627e9ed131126df6111dcb1c302c0
Anthony Tsang <anthonytsang0226@gmail.com>
2023-09-23 14:50:00 +0200

    feat(cpp05): init

---
+++ ex02/main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 20:44:18 by htsang            #+#    #+#             */
+/*   Updated: 2023/09/23 14:33:54 by htsang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+
+#include <iostream>
+#include <string>
+
+namespace printer
+{
+  void  InteractivePrompt()
+  {}
+} // namespace printer
+
+namespace interactive
+{
+  int Run()
+  {
+    std::string input;
+
+    printer::InteractivePrompt();
+    std::getline(std::cin, input);
+    while (std::cin.good())
+    {
+      printer::InteractivePrompt();
+      std::getline(std::cin, input);
+    }
+    return EXIT_SUCCESS;
+  }
+} // namespace battle
+
+namespace noninteractive
+{
+  int Run()
+  {
+    return EXIT_SUCCESS;
+  }
+} // namespace noninteractive
+
+int main(int argc, const char** argv)
+{
+  if (argc == 1)
+    return noninteractive::Run();
+  else if (argc == 2 && std::string(argv[1]) == "-i")
+    return interactive::Run();
+  else
+  {
+    std::cerr << "Usage: " << argv[0] << " [-i]" << std::endl;
+    return EXIT_FAILURE;
+  }
+}
