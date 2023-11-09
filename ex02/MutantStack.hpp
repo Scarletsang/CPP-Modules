e99177912eb6840b68473511b69c999d89d3deec
Anthony Tsang <htsang@student.42heilbronn.de>
2023-11-09 23:46:56 +0100

    feat: completed ex02 without test

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,23 +6,70 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 23:01:50 by htsang            #+#    #+#             */
-/*   Updated: 2023/11/09 23:03:08 by htsang           ###   ########.fr       */
+/*   Updated: 2023/11/09 23:42:43 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <stack>
+#include <deque>
 
-template <typename T>
-class MutantStack : std::stack<T>
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
   public:
+    typedef typename Container::iterator iterator;
     MutantStack();
     MutantStack(const MutantStack& copy);
     ~MutantStack();
 
     MutantStack& operator=(const MutantStack& copy);
 
-    
+    iterator begin();
+    iterator end();
+
+    void  push_back(const T& value);
 };
+
+/////////////////////////////////////////////////////
+////////////   template implementation   ////////////
+/////////////////////////////////////////////////////
+
+template <typename T, typename Container>
+MutantStack<T, Container>::MutantStack()
+  : std::stack<T, Container>() {}
+
+template <typename T, typename Container>
+MutantStack<T, Container>::MutantStack(const MutantStack& copy)
+  : std::stack<T, Container>(copy) {}
+
+template <typename T, typename Container>
+MutantStack<T, Container>::~MutantStack() {}
+
+template <typename T, typename Container>
+MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack& copy)
+{
+  if (this == &copy)
+    return *this;
+  std::stack<T, Container>::operator=(copy);
+  return *this;
+}
+
+template <typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin()
+{
+  return this->c.begin();
+}
+
+template <typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
+{
+  return this->c.end();
+}
+
+template <typename T, typename Container>
+void  MutantStack<T, Container>::push_back(const T& value)
+{
+  this->c.push_back(value);
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 20:44:18 by htsang            #+#    #+#             */
-/*   Updated: 2023/11/05 18:31:42 by htsang           ###   ########.fr       */
+/*   Updated: 2023/11/09 23:46:11 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 #include <iostream>
 #include <string>
+#include <list>
+#include <stack>
 
 #include "InteractivePrompt.hpp"
+#include "MutantStack.hpp"
 
 namespace interactive
 {
@@ -40,12 +43,80 @@ namespace interactive
     prompt.shell(states);
     return EXIT_SUCCESS;
   }
-} // namespace battle
+} // namespace interactive
+
+namespace test
+{
+  int  TestWithMutantStack()
+  {
+    MutantStack<int>    mstack;
+    mstack.push_back(5);
+    mstack.push_back(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
+    //[...]
+    mstack.push_back(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while(it != ite){
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  int TestWithList()
+  {
+    std::list<int>    list;
+    list.push_back(5);
+    list.push_back(17);
+    std::cout << list.back() << std::endl;
+    list.pop_back();
+    std::cout << list.size() << std::endl;
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    //[...]
+    list.push_back(0);
+    std::list<int>::iterator it = list.begin();
+    std::list<int>::iterator ite = list.end();
+    ++it;
+    --it;
+    while(it != ite){
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  int CheckMutantStackInheritance()
+  {
+    MutantStack<int>    mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::stack<int> s(mstack);
+    return EXIT_SUCCESS;
+  }
+} // namespace test
 
 namespace noninteractive
 {
   int Run()
   {
+    test::TestWithMutantStack();
+    std::cout << std::endl;
+    test::TestWithList();
+    if (test::CheckMutantStackInheritance() == EXIT_FAILURE)
+    {
+      std::cerr << "MutantStack does not inherit from std::stack" << std::endl;
+      return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
   }
 } // namespace noninteractive
