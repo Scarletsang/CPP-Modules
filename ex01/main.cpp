d2978128b9fc103af29332a5e38eed459f326459
Anthony Tsang <htsang@student.42heilbronn.de>
2023-12-06 16:17:53 +0100

    feat(ex01): Completed the program. Still have bugs

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 20:44:18 by htsang            #+#    #+#             */
-/*   Updated: 2023/12/06 00:38:13 by htsang           ###   ########.fr       */
+/*   Updated: 2023/12/06 16:16:05 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,72 @@
 
 #include "RPN.hpp"
 #include "Error.hpp"
+#include "Result.hpp"
 
-Error ParseExpression(std::stringstream& ss, RPN& rpn)
+typedef Result<RPN::Operation, Error> OperationResult;
+typedef Result<int, Error>            IntResult;
+typedef Result<Nothing, Error>        NoResult;
+
+IntResult  ParseNumber(std::stringstream& ss)
 {
   int number;
+  ss >> number;
+  if (ss.fail())
+  {
+    ss.clear();
+    return IntResult::Error(Error(Error::kInvalidNumber));
+  }
+  return IntResult::Ok(number);
+}
 
+NoResult  PushNumber(int number, RPN& rpn)
+{
+  rpn.push(number);
+  return NoResult::Ok(Nothing());
+}
+
+OperationResult ParseOperator(std::stringstream& ss)
+{
+  char op = ss.peek();
+
+  switch (op)
+  {
+    case '+':
+      ss.get();
+      return OperationResult::Ok(RPN::kAdd);
+    case '-':
+      ss.get();
+      return OperationResult::Ok(RPN::kSub);
+    case '*':
+      ss.get();
+      return OperationResult::Ok(RPN::kMul);
+    case '/':
+      ss.get();
+      return OperationResult::Ok(RPN::kDiv);
+    default:
+      return OperationResult::Error(Error(Error::kInvalidOperand));
+  }
+}
+
+NoResult  ApplyOperation(RPN::Operation operation, RPN& rpn)
+{
+  if (rpn.apply(operation))
+    return NoResult::Error(Error(Error::kInvalidOperation));
+  return NoResult::Ok(Nothing());
+}
+
+NoResult ParseExpression(std::stringstream& ss, RPN& rpn)
+{
   while (ss.good())
   {
-    // TODO
+    NoResult result = ParseNumber(ss).chain(PushNumber, rpn);
+    if (result.is_ok())
+      continue;
+    result = ParseOperator(ss).chain(ApplyOperation, rpn);
+    if (!result.is_ok())
+      return NoResult::Error(result.error());
   }
+  return NoResult::Ok(Nothing());
 }
 
 int main(int argc, const char** argv)
@@ -38,21 +95,20 @@ int main(int argc, const char** argv)
   }
   RPN               rpn;
   std::stringstream ss;
-  Error             err;
   for (int i = 0; i < argc; i++)
   {
     ss << argv[i];
-    err = ParseExpression(ss, rpn);
-    if (err.code() != Error::kOk)
+    NoResult parse_result = ParseExpression(ss, rpn);
+    if (!parse_result.is_ok())
     {
-      std::cout << err << std::endl;
+      std::cout << parse_result.error() << std::endl;
       return EXIT_FAILURE;
     }
   }
   switch (rpn.size())
   {
     case 1:
-      std::cout << rpn.top() << std::endl;
+      std::cout << rpn.top().value() << std::endl;
       return EXIT_SUCCESS;
     case 0:
       std::cout << Error(Error::kStackEmpty) << std::endl;
